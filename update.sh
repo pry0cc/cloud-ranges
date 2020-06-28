84b5b8170ad78b36dc6ce9811a96f1fa0c522189
pry0cc <pry0cc@0x00sec.org>
2020-06-28 22:09:39 +0000

    Updated at&t at 06/28/20-22:09:39 EST

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ do
         rm -rf companies/$company
         proxychains4 -q ./asn
         proxychains4 -q ./asn
+        echo "all" > index.html && for i in $(bash -c "ls companies/"); do cp companies/$i/ranges.txt companies/$i/index.html; echo "$i" >> index.html; done
         git add .
         git commit -m "Updated $company at $(date +%D-%T) EST"
         git pull
