08aae11052dc5f652911822de9a975be196a77b7
pry0cc <pry0cc@0x00sec.org>
2020-06-28 16:30:52 -0400

    added update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-while true; do for company in $(bash -c "ls companies/"); do rm -rf companies/$company; proxychains -q ./asn; proxychains -q ./asn; git add .; git commit -m "Updated $company at $(date +%D-%T) EST"; git push; sleep 120; done; done
+while true
+do 
+    for company in $(bash -c "ls companies/") 
+    do 
+        rm -rf companies/$company
+        proxychains -q ./asn
+        proxychains -q ./asn
+        git add .
+        git commit -m "Updated $company at $(date +%D-%T) EST"
+        git push
+        sleep 120
+    done
+    echo "Sleeping for 86400 seconds (24 hours!) See you tomorrow!"
+    sleep 86400
+done
